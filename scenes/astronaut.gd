bc360bd49325e62c082364661f15969d4d92a0e8
valgguk <valrj.college.study@gmail.com>
2026-04-08 16:19:53 -0400

    Refactor scene files: update resource paths and add player movement actions

--- scenes/astronaut.gd
+++ scenes/astronaut.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
 @export var color: int = 0
+@onready var label: Label = $Label
 
 const SPEED = 400.0
 const JUMP_VELOCITY = -900.0
@@ -19,12 +20,11 @@ func _physics_process(delta):
 		velocity += get_gravity() * Gravity_factor* delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 		change_sprite_direction(direction)
@@ -64,3 +64,8 @@ func walking_wobble(direction):
 	tween.tween_property(animated_sprite,"rotation",0,0.05)
 	await tween.finished 
 	walking=false
+	
+	
+func setup(data: Statics.PlayerData) -> void:
+	name = str(data.id)
+	label.text = data.name
