c0cadb246d14b2c3abb01be630fe49503700df2f
valgguk <valrj.college.study@gmail.com>
2026-05-13 21:48:27 -0400

    feat(multiplayer): profe help, resolve bugs - enhance input synchronization - rotation replication

--- scenes/Lever.gd
+++ scenes/Lever.gd
@@ -12,9 +12,9 @@ func _process(_delta: float) -> void:
 		return
 	
 	if Input.is_action_just_pressed("area_interact"):
-		_get_main().input_rotation.rpc(direction)
+		_get_main().input_rotation.rpc_id(1,direction)
 	elif Input.is_action_just_released("area_interact"):
-		_get_main().input_rotation.rpc(0)
+		_get_main().input_rotation.rpc_id(1,0)
 
 func _on_body_entered(body: Node) -> void:
 	if body is CharacterBody2D and body.is_multiplayer_authority():
@@ -23,7 +23,7 @@ func _on_body_entered(body: Node) -> void:
 func _on_body_exited(body: Node) -> void:
 	if body is CharacterBody2D and body.is_multiplayer_authority():
 		player_inside = false
-		_get_main().input_rotation.rpc(0)
+		_get_main().input_rotation.rpc_id(1,0)
 
 func _get_main() -> Node:
 	return get_tree().get_root().get_node("Main")
