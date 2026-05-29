0d4c5e6afc9d949f1341180c0aa46e3a8d5a0081
XavierS <xavier.sepulveda@ug.uchile.cl>
2026-05-28 20:27:52 -0400

    feat: Icon added

---
+++ scripts/score_tracker.gd
@@ -0,0 +1,13 @@
+extends Label
+var score= 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+
+func _on_timer_timeout():
+	score+=10
+	text = "SCORE: " + str(score)
