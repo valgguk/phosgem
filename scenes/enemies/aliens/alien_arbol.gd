92d54bdfe5ac7b8cb5f25cde9b227b43341f1a4b
valgguk <valrj.college.study@gmail.com>
2026-06-12 21:22:07 -0400

    feat: enhance hitbox and hurtbox components with damage handling and owner reference - resuelvo bug con detección y aplicación en targets

--- scenes/enemies/aliens/alien_arbol.gd
+++ scenes/enemies/aliens/alien_arbol.gd
@@ -17,12 +17,20 @@ var ship_velocity: Vector2 = Vector2.ZERO
 # tipo : sigue a un player 
 var target: Node2D = null
 var already_hit := {}
+var hit_cooldown := 1.0
 
-var players: Array = []
-var current_target_index := 0
+#var players: Array = []
+#var current_target_index := 0
 enum State { CHASE, COOLDOWN }
 var state: State = State.CHASE
 
+var target_refresh_time := 0.4
+var target_timer := 0.0
+var target_switch_threshold := 40.0 # distancia mínima para cambiar target
+
+var aggro_target: Node2D = null
+var aggro_time := 2.0
+var aggro_timer := 0.0
 
 func _ready() -> void:
 	add_to_group("aliens_instances")
@@ -31,7 +39,7 @@ func _ready() -> void:
 	hitbox_component.damage_dealt.connect(_on_damage_dealt)
 	#sync_timer.timeout.connect(_on_sync_timeout)
 	cooldown_timer.timeout.connect(_on_cooldown_timeout)
-	_update_players()
+	_update_target()
 	multiplayer.peer_connected.connect(_on_peer_changed)
 	multiplayer.peer_disconnected.connect(_on_peer_changed)
 
@@ -44,6 +52,17 @@ func _physics_process(delta: float) -> void:
 		var vertical_direction = g.normalized()
 		var horizontal_direction = vertical_direction.rotated(-PI/2)
 		
+		aggro_timer -= delta
+		if aggro_target and aggro_timer > 0:
+			target = aggro_target
+		else:
+			_update_target()
+		
+		target_timer -= delta
+		if target_timer <= 0:
+			_update_target()
+			target_timer = target_refresh_time
+			
 		match state:
 			State.CHASE:
 				_move_to_target()
@@ -70,8 +89,6 @@ func _physics_process(delta: float) -> void:
 		
 		# var angle = vertical_direction.angle() + PI/2
 		# pivote.rotation = angle
-	
-		
 		move_and_slide()
 		var collision = get_last_slide_collision()
 		if collision:
@@ -88,25 +105,33 @@ func _physics_process(delta: float) -> void:
 	
 	
 func take_damage(damage: int) -> void:
-	Debug.log("%d player->alien" % damage)
+	Debug.log("Mario hit %d" % damage)
 	
 
 func _on_health_changed(value: int) -> void:
 	take_damage(value)
 	
-func _on_damage_dealt():
+func _on_damage_dealt(body_target: CharacterBody2D):
 	if not is_multiplayer_authority():
 		return
-	if not target:
+	if not body_target:
 		return
-	var id = target.get_multiplayer_authority()
+	if body_target == self:
+		return	
+		
+	aggro_target = body_target
+	aggro_timer = aggro_time
+		
+	#var id = body_target.get_multiplayer_authority()
+	var id = body_target.get_instance_id()
+	
+	# aplicar stun desde server
 	if id in already_hit:
 		return
 	
 	already_hit[id] = true
 	
-	# aplicar stun desde server
-	target.apply_stun.rpc()
+	body_target.apply_stun.rpc()
 	
 	if state != State.CHASE:
 		return
@@ -120,21 +145,23 @@ func _on_damage_dealt():
 	cooldown_timer.start()
 	
 # usar nuevamente
-#func _find_closest_player(exclude: Node2D = null) -> Node2D:
-	#var players = get_tree().get_nodes_in_group("players_instances")
-	## print("players encontrados:", players)
-	#var closest = null
-	#var min_dist = INF
-	#
-	#for p in players:
-		#if p == exclude:
-			#continue
-		#var dist = global_position.distance_to(p.global_position)
-		#if dist < min_dist:
-			#min_dist = dist
-			#closest = p
-	#
-	#return closest
+func _find_closest_player(exclude: Node2D = null) -> Node2D:
+	var players = get_tree().get_nodes_in_group("players_instances")
+	# print("players encontrados:", players)
+	var closest: Node2D = null
+	var min_dist := INF
+	
+	for p in players:
+		if p == exclude:
+			continue
+		if not is_instance_valid(p):
+			continue
+		var dist = global_position.distance_to(p.global_position)
+		if dist < min_dist:
+			min_dist = dist
+			closest = p
+	
+	return closest
 	
 func _should_jump() -> bool:
 	if not target:
@@ -176,60 +203,85 @@ func _on_sync_timeout() -> void:
 		send_position.rpc(position)
 		
 func _on_cooldown_timeout():
-	_next_target()
+	already_hit.clear()
 	state = State.CHASE
 
 @rpc("authority", "call_remote", "unreliable_ordered")
 func sync_direction(dir: float):
 	move_direction = dir
 	
-func _next_target():
-	if players.size() == 0:
-		target = null
-		return
-		
-	already_hit.clear()
-	
-	current_target_index = (current_target_index + 1) % players.size()
-	target = players[current_target_index]
-	sync_target.rpc(current_target_index)
+#func _next_target():
+	#if players.size() == 0:
+		#target = null
+		#return
+		#
+	#already_hit.clear()
+	#
+	#current_target_index = (current_target_index + 1) % players.size()
+	#target = players[current_target_index]
+	#sync_target.rpc(current_target_index)
 	
 func _move_to_target():
 	if not target:
 		return
+		
+	if not is_instance_valid(target):
+		target = null
+		return
 	
 	var dir = target.global_position - global_position
 	
-	if abs(dir.x) < 10:
-		return # no cambies dirección
 	var horizontal = get_gravity().normalized().rotated(-PI/2)
 	var d = dir.dot(horizontal)
-	if abs(d) > 5:
+	
+	var deadzone := 10
+	if abs(d) > deadzone:
 		move_direction = sign(d)
+	#if abs(dir.x) < 10:
+		#return # no cambies dirección
 	
 func _move_away():
 	if not target:
 		return
 	
-	var dir = (global_position - target.global_position).normalized()
+	var dir = global_position - target.global_position
 	var horizontal = get_gravity().normalized().rotated(-PI/2)
 	var d = dir.dot(horizontal)
 	if abs(d) > 5:
 		move_direction = sign(d)
 	
-@rpc("authority", "call_remote", "reliable")
-func sync_target(index: int):
-	current_target_index = index
-	
-	if players.size() > index:
-		target = players[index]
+#@rpc("authority", "call_remote", "reliable")
+#func sync_target(index: int):
+	#current_target_index = index
+	#
+	#if players.size() > index:
+		#target = players[index]
 		
-func _update_players():
-	await get_tree().process_frame
-	players = get_tree().get_nodes_in_group("players_instances")
-	players.sort_custom(func(a,b): return a.get_multiplayer_authority() < b.get_multiplayer_authority())
-	if players.size() > 0:
-		target = players[0]
+#func _update_players():
+	#await get_tree().process_frame
+	#players = get_tree().get_nodes_in_group("players_instances")
+	#players.sort_custom(func(a,b): return a.get_multiplayer_authority() < b.get_multiplayer_authority())
+	#if players.size() > 0:
+		#target = players[0]
 		
 func _on_peer_changed(_id):
-	_update_players()
+	_update_target()
+	#_update_players()
+	
+func _update_target():
+	var new_target = _find_closest_player()
+
+	if not new_target:
+		target = null
+		return
+	
+	if not target:
+		target = new_target
+		return
+	
+	var current_dist = global_position.distance_to(target.global_position)
+	var new_dist = global_position.distance_to(new_target.global_position)
+	
+	# solo cambia si realmente es mejor
+	if new_dist + target_switch_threshold < current_dist:
+		target = new_target
