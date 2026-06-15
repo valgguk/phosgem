c110855f58023d4db5c30f37857662bd1da93f32
XavierS <xavier.sepulveda@ug.uchile.cl>
2026-06-14 23:38:55 -0400

    refactor: deleted old unused files and organized proyect

--- scripts/energy_generator.gd
+++ scripts/energy_generator.gd
@@ -1,17 +1,19 @@
 extends Area2D
+@onready var progress_bar = $ProgressBar
 
 
 var player_inside := false
 var player_running := false
 @export var energy: int
-@export var energy_generation_per_second: int
-@export var energy_lost_per_second: int
+@export var energy_generation_per_second: int = 1
+@export var energy_lost_per_second: int = 1
 @export var MAX_ENERGY = 100
 
 	
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
+	energy= MAX_ENERGY
 
 func _process(_delta: float) -> void:
 	if not player_inside:
@@ -24,11 +26,11 @@ func _process(_delta: float) -> void:
 		
 
 func _on_body_entered(body: Node) -> void:
-	if body is CharacterBody2D and body.is_multiplayer_authority():
+	if body is CharacterBody2D:
 		player_inside = true
 
 func _on_body_exited(body: Node) -> void:
-	if body is CharacterBody2D and body.is_multiplayer_authority():
+	if body is CharacterBody2D:
 		player_inside = false
 
 func _get_main() -> Node:
@@ -37,10 +39,13 @@ func _get_main() -> Node:
 
 func change_energy(amount: int):
 	energy = clampi(energy+amount,0,MAX_ENERGY)
+	progress_bar.value =energy
+
 	
 
 func _on_timer_timeout():
 	if not player_inside:
+
 		change_energy(-energy_lost_per_second)
 		return
 	change_energy(energy_generation_per_second)
