2d66f7ad56e7bd761ccf5d37a08b5f3b48d399c6
valgguk <valrj.college.study@gmail.com>
2026-07-02 15:23:35 -0400

    feat: implement AlienBullet scene and firing mechanics in AlienShip - Ahora la nave de alien dispara!

---
+++ scenes/enemies/aliens/alien_bullet.gd
@@ -0,0 +1,16 @@
+extends HitboxComponent
+
+@export var max_speed = 400
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	var velocity = transform.x * max_speed
+	position += velocity * delta
--- scenes/enemies/aliens/alien_ship.gd
+++ scenes/enemies/aliens/alien_ship.gd
@@ -4,6 +4,11 @@ extends CharacterBody2D
 @onready var playback: AnimationNodeStateMachinePlayback = animation_tree["parameters/playback"]
 @onready var health_component: HealthComponent = $HurtboxComponent/HealthComponent
 
+@export var bullet_scene : PackedScene
+@onready var bullet_spawn: Marker2D = $BulletSpawn
+#@export var munition_max = 5
+#var munition_now = munition_max
+
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -14,23 +19,18 @@ func _ready() -> void:
 	playback.travel("idle")
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+	#if munition_now == 0:
+		##recarga
+		#munition_now = munition_max
+		#await get_tree().create_timer(5).timeout
+		#return
+		#
+	#while munition_now > 0:
+		#fire()
+		#await get_tree().create_timer(3).timeout
+		#munition_now -= 1
+	pass
+	
 	
 	
 func take_damage(damage: int) -> void:
@@ -51,3 +51,9 @@ func die():
 	playback.travel("die")
 	await get_tree().create_timer(0.4).timeout
 	call_deferred("queue_free")
+	
+func fire() -> void:
+	var bullet_inst = bullet_scene.instantiate()
+	add_child(bullet_inst)
+	bullet_inst.global_position = bullet_spawn.global_position
+	bullet_inst.global_rotation = Vector2.LEFT.angle()
