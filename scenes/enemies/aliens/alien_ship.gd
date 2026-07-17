d38cf4a1abf9f34085f916c2c87bb27ec53e46d7
valgguk <valrj.college.study@gmail.com>
2026-07-17 19:29:29 -0400

    feat: add game over scene and enhance UI elements - arreglos merge 1

--- scenes/enemies/aliens/alien_ship.gd
+++ scenes/enemies/aliens/alien_ship.gd
@@ -9,8 +9,8 @@ extends CharacterBody2D
 @export var alien_to_spawn: PackedScene
 
 @export var shoot_interval := 60.0 # 60 segundos / 120.0 para 2 min
-@export var min_interval := 20.0
-@export var max_interval := 30.0
+@export var min_interval := 10.0
+@export var max_interval := 15.0
 @onready var shoot_timer: Timer = $ShootTimer
 
 const SPEED = 300.0
